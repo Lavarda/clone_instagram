40b58049fec0e5868f1ba5f198b02b077d5245dc
vlavarda <vlavarda@aawz.com.br>
2019-06-14 18:49:29 -0300

    Finalizaçao da API para back-end.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Projeto Clone Instagram
 
 Projeto Clone Instagram
+
+- Instalar configurações para rodar o back-end do projeto :
+    yarn install 
--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,13 +1,24 @@
 // importações
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect('mongodb+srv://Lavarda:vi2107@cluster0-g6df3.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 });
 
+app.use( (req, res, next ) => {
+    req.io = io;
+    next();
+})
+app.use(cors());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes.js'));
 
 // Setando a porta do servidor
-app.listen(3333);
+server.listen(3333);
